0c6c4c0d92602d7b4416513053b927a170116eb0
listopadgeorgii <listopadiogl@gmail.com>
2022-12-22 23:46:31 +0300

    export | import

---
+++ src/tech.js
@@ -0,0 +1,13 @@
+const Tech = () => {
+
+    return <div>
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    </div>
+}
+
+export default Tech;
